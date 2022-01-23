302467f6f635c27a06412135caadf3b7f911517c
niiwade <niilantewade77@gmail.com>
2022-01-23 21:18:37 +0000

    changed tailwind config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
+  mode: "jit",
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
   content: [],
   theme: {
     extend: {},
+    backgroundImage: {
+      'hero-bg': "url('/cbg.svg')",
+      'hero-bg2': "url('/bg2.svg')"
+    },
   },
   plugins: [],
 }
